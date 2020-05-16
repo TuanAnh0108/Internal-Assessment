9356de47fcf204e51d2504d0608154f17b6da02c
TuanAnh0108 <57340581+TuanAnh0108@users.noreply.github.com>
2020-05-16 08:02:07 +0700

    Update Defibrillators.py

--- Week34/Defibrillators.py
+++ Week34/Defibrillators.py
@@ -10,38 +10,38 @@ n = int(input())
 info = []
 info_1 = []
 info_2 = []
-name_data = []
-lon_data_string = []
-lat_data_string = []
-lon_data = []
-lat_data = []
+name_data = []  # Store the name data of the place
+lon_data_string = []  # Store the data of the longtitude (string type)
+lat_data_string = []  # Store the data of the latitude (string type)
+lon_data = []  # Store the data of the longtitude 
+lat_data = []  # Store the data of the latitude
 X = []
 Y = []
-D = []
+D = []  # Store the distance data for each place
 
-lon_0 = float(lon.replace(",", "."))
+lon_0 = float(lon.replace(",", "."))  # Convert string type to float type
 lat_0 = float(lat.replace(",", "."))
 
-for i in range(n):
+for i in range(n):  # Append all the input data into a list
     defib = input()
     info.append(defib)
 
-for i in range(len(info)):
+for i in range(len(info)):  # Split elements in the list by ";"
     word1 = info[i].split(";")
     info_1.append(word1)
 
 for i in range(len(info_1)):
-    lon_data_string.append(info_1[i][4])
-    lat_data_string.append(info_1[i][5])
-    name_data.append(info_1[i][1])
+    lon_data_string.append(info_1[i][4])  # Append the longtitude (string type) data
+    lat_data_string.append(info_1[i][5])  # Append the latitude (string type) data
+    name_data.append(info_1[i][1])  # Append name data of each place
 
-
-for i in range(len(lon_data_string)):
+for i in range(len(lon_data_string)):  # Convert string data to float data
     lon_1 = float(lon_data_string[i].replace(",", "."))
     lon_data.append(lon_1)
     lat_1 = float(lat_data_string[i].replace(",", "."))
     lat_data.append(lat_1)
 
+# Calculate the distance
 for i in lon_data:
     x = (i - lon_0) * math.cos((i + lon_0) / 2)
     X.append(x)
@@ -54,6 +54,8 @@ for i, j in zip(X, Y):
     d = math.sqrt(i * i + j * j) * 6371
     D.append(d)
 
+# Find the index of the nearest place
 index = D.index(min(D))
 
+# Print out the result
 print(name_data[index])
